61505c4701879f51158063fb0bebf0d0bb931299
Omar <omar.ashi@hotmail.com>
2020-10-10 23:00:58 +0400

    Add click handler for meals overview screen

--- lib/screens/meals_overview_screen.dart
+++ lib/screens/meals_overview_screen.dart
@@ -4,6 +4,7 @@ import '../widgets/image_header.dart';
 import '../widgets/horizontal_list.dart';
 import '../widgets/most-ordered.dart';
 import '../widgets/burger_card.dart';
+import './meel_detail_screen.dart';
 
 class MealsOverview extends StatefulWidget {
   @override
@@ -11,6 +12,14 @@ class MealsOverview extends StatefulWidget {
 }
 
 class _MealsOverviewState extends State<MealsOverview> {
+  cardClicked(burgerName) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => MealDetail(burgerName),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -22,29 +31,29 @@ class _MealsOverviewState extends State<MealsOverview> {
           Container(
             alignment: Alignment.centerRight,
             child: BurgerCard(
-              imageLocation: "assets/images/burger2.png",
-              burgerPrice: "AED 39",
-              burgerName: "B-b-bacon!",
-              burgerCaption: "Juicy just got juicier.",
-            ),
+                imageLocation: "assets/images/burger2.png",
+                burgerPrice: "AED 39",
+                burgerName: "B-b-bacon!",
+                burgerCaption: "Juicy just got juicier.",
+                cardClicked: cardClicked),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               BurgerCard(
-                imageLocation: "assets/images/burger4.png",
-                burgerPrice: "AED 42",
-                burgerName: "ShackBurger*",
-                burgerCaption: "Cheesy and Juicy.",
-              ),
+                  imageLocation: "assets/images/burger4.png",
+                  burgerPrice: "AED 42",
+                  burgerName: "ShackBurger*",
+                  burgerCaption: "Cheesy and Juicy.",
+                  cardClicked: cardClicked),
               BurgerCard(
-                imageLocation: "assets/images/burger3.png",
-                burgerPrice: "AED 39",
-                burgerName: "B-b-bacon!",
-                burgerCaption: "Juicy just got juicier.",
-              ),
+                  imageLocation: "assets/images/burger3.png",
+                  burgerPrice: "AED 39",
+                  burgerName: "B-b-bacon!",
+                  burgerCaption: "Juicy just got juicier.",
+                  cardClicked: cardClicked),
             ],
-          )
+          ),
         ],
       ),
     );
