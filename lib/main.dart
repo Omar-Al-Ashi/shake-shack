ee1095af57e425201e5cee088ad26ad09d44fadd
Omar <omar.ashi@hotmail.com>
2020-10-10 23:00:26 +0400

    extract appBar to external widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import "./screens/meals_overview_screen.dart";
+import "./widgets/custom_app_bar.dart";
 
 void main() => runApp(MyApp());
 
@@ -19,54 +20,8 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          color: Colors.black,
-          onPressed: () {},
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Image.asset('assets/icons/mic_icon.png'),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.search),
-            color: Colors.black,
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Image.asset('assets/icons/dials_icon.png'),
-            onPressed: () {},
-          ),
-        ],
-        title: Column(
-          children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Order To",
-                style: TextStyle(
-                  color: Color(0xff55A335),
-                  fontSize: 11,
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Jumeirah Lake Towers",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ],
-        ),
+      appBar: CustomAppBar(
+        isBack: false,
       ),
       body: MealsOverview(),
     );
