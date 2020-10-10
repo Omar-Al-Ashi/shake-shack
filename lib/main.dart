198d9b65bf49554be59a752e385d9de1c9b2a2da
Omar <omar.ashi@hotmail.com>
2020-10-10 12:57:34 +0400

    Set Futura as default fontFamily in main

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shack Shack',
+      theme: ThemeData(fontFamily: 'Futura'),
       home: MyHomePage(),
     );
   }
