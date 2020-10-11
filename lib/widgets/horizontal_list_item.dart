d386e333dfbd5ebb3362c683b136f74efdf2b112
Omar <omar.ashi@hotmail.com>
2020-10-11 12:49:19 +0400

    Convert horizontal list item to stateful widget

--- lib/widgets/horizontal_list_item.dart
+++ lib/widgets/horizontal_list_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HorizontalListItem extends StatelessWidget {
+class HorizontalListItem extends StatefulWidget {
   final String iconLocation;
   final int itemId;
   final bool isItemClicked;
@@ -10,26 +10,32 @@ class HorizontalListItem extends StatelessWidget {
     this.iconLocation,
     this.itemId,
     this.itemClicked,
-    this.isItemClicked,
+    this.isItemClicked = false,
   });
 
   @override
+  _HorizontalListItemState createState() => _HorizontalListItemState();
+}
+
+class _HorizontalListItemState extends State<HorizontalListItem> {
+  @override
   Widget build(BuildContext context) {
+    print(widget.isItemClicked);
     return InkWell(
-      onTap: () => itemClicked(itemId),
+      onTap: () => widget.itemClicked(widget.itemId),
       child: Container(
         margin: EdgeInsets.all(10),
         height: 50,
         width: 50,
         decoration: BoxDecoration(
           border: Border.all(
-            color: isItemClicked ? Colors.green : Colors.black,
+            color: widget.isItemClicked ? Colors.green : Colors.black,
             width: 1,
           ),
           borderRadius: BorderRadius.circular(15),
           color: Colors.white,
         ),
-        child: Image.asset(iconLocation),
+        child: Image.asset(widget.iconLocation),
       ),
     );
   }
