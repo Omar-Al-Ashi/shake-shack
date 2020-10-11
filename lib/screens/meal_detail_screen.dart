4dc2834a539174def3da4e467f1d134fdbca6dc3
Omar <omar.ashi@hotmail.com>
2020-10-11 12:48:02 +0400

    Update meal detail screen to add new sections

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+import '../widgets/extras_list.dart';
+import '../widgets/horizontal_list_item.dart';
 import '../widgets/meal_detail_header.dart';
 import '../widgets/meal_detail_info.dart';
 
@@ -8,6 +13,68 @@ class MealDetail extends StatelessWidget {
   MealDetail(this.title);
   @override
   Widget build(BuildContext context) {
+    final List icons = [
+      {
+        "iconId": 1,
+        "iconLocation": "assets/icons/cheese_icon.png",
+        "isSelected": false,
+      },
+      {
+        "iconId": 2,
+        "iconLocation": "assets/icons/cucumber_icon.png",
+        "isSelected": false,
+      },
+      {
+        "iconId": 3,
+        "iconLocation": "assets/icons/jalapenos_icon.png",
+        "isSelected": false,
+      },
+      {
+        "iconId": 4,
+        "iconLocation": "assets/icons/lettuce_icon.png",
+        "isSelected": false,
+      },
+      {
+        "iconId": 5,
+        "iconLocation": "assets/icons/onion_icon.png",
+        "isSelected": false,
+      },
+      {
+        "iconId": 6,
+        "iconLocation": "assets/icons/tomato_icon.png",
+        "isSelected": false,
+      },
+    ];
+    final List<dynamic> extras = [
+      {
+        "id": 1,
+        "title": "Add Bacon (70 kcal)",
+        "price": "AED 5",
+        "isSelected": false,
+      },
+      {
+        "id": 2,
+        "title": "Add Cheese (120 kcal)",
+        "price": "AED 3",
+        "isSelected": false,
+      },
+      {
+        "id": 3,
+        "title": "No Sauce (-85 kcal)",
+        "price": "AED 0",
+        "isSelected": false,
+      },
+    ];
+
+    toggleIsSelected(item) {
+      item['isSelected'] != item['isSelected'];
+    }
+
+    extraClickedHandler(id) {
+      var item = extras.firstWhere((element) => element['id'] == id);
+      toggleIsSelected(item);
+    }
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -21,39 +88,142 @@ class MealDetail extends StatelessWidget {
         backgroundColor: Colors.white,
       ),
       body: Container(
-        child: Column(
+        child: ListView(
           children: [
-            MealDetailHeader(),
-            MealDetailInfo(),
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+            Column(
+              children: [
+                MealDetailHeader(),
+                MealDetailInfo(),
+                Column(
                   children: [
-                    Text("Patty"),
-                    Container(
-                      height: 95,
-                      width: 165,
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 1,
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        child: Text(
+                          "Patty",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
                         ),
                       ),
-                      child: Column(
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        height: 95,
+                        width: 165,
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Double",
+                              style: TextStyle(
+                                fontFamily: 'Futura_Bold',
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                            Text(
+                              "864 kcal",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        child: Text(
+                          "Ingredients",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Wrap(
+                        direction: Axis.horizontal,
+                        alignment: WrapAlignment.start,
                         children: [
-                          Text("Double"),
-                          Text("864 Cal"),
+                          ...icons
+                              .map(
+                                (icon) => HorizontalListItem(
+                                  iconLocation: icon['iconLocation'],
+                                  itemId: icon['iconId'],
+                                  isItemClicked: icon['isSelected'],
+                                  itemClicked: () => {},
+                                ),
+                              )
+                              .toList(),
                         ],
                       ),
                     ),
                   ],
                 ),
-              ),
+                Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        padding: EdgeInsets.all(20),
+                        child: Text(
+                          "Extras",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Wrap(
+                        direction: Axis.horizontal,
+                        alignment: WrapAlignment.start,
+                        children: [
+                          ExtrasList(
+                            extras: extras,
+                            extraClickedHandler: extraClickedHandler,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [],
+                )
+              ],
             ),
           ],
         ),
