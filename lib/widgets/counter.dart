acfa9adf518b78eaf4b8352f2ac834ab68efb62a
Omar <omar.ashi@hotmail.com>
2020-10-11 21:59:23 +0400

    Update counter widget

--- lib/widgets/counter.dart
+++ lib/widgets/counter.dart
@@ -12,9 +12,10 @@ class _CounterState extends State<Counter> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
+          color: Color(0xffF4F4F4),
         border: Border.all(
           color: Colors.black,
-          width: 1,
+          width: 1.5
         ),
         borderRadius: BorderRadius.circular(12),
       ),
@@ -24,6 +25,7 @@ class _CounterState extends State<Counter> {
             height: 46,
             width: 37,
             child: RaisedButton(
+              color: Colors.white,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(12),
               ),
@@ -36,13 +38,17 @@ class _CounterState extends State<Counter> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 20,
+            ),
             child: Text(counter.toString()),
           ),
           Container(
             height: 46,
             width: 37,
             child: RaisedButton(
+              color: Colors.white,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(12),
               ),
