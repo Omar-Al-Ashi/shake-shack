0f0cef8bbef5fc3e4036b0425ae5763dbdc6b73e
Omar <omar.ashi@hotmail.com>
2020-10-11 21:58:00 +0400

    Add overlay screen

---
+++ lib/overlays/cart-overlay.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:shake_shack/widgets/checkbox_list_tile_list.dart';
+import '../widgets/cart_list.dart';
+
+class CartOverlay extends StatelessWidget {
+  final Function exitCartOverlay;
+
+  final List checkboxListTileList = [
+    "Save Instructions",
+    "No Cutlery",
+    "Contactless Delivery"
+  ];
+
+  CartOverlay(this.exitCartOverlay);
+
+  @override
+  Widget build(BuildContext context) {
+    var deviceHeight = MediaQuery.of(context).size.height;
+    var deviceWidth = MediaQuery.of(context).size.width;
+    return ListView(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.black87,
+          ),
+          height: deviceHeight - 100,
+          width: deviceWidth - 100,
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Your Cart",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "4 items",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      icon: new Icon(Icons.arrow_downward),
+                      color: Colors.white,
+                      onPressed: () => exitCartOverlay(),
+                    ),
+                  ],
+                ),
+                CartList(),
+                SizedBox(
+                  height: 15,
+                ),
+                const Divider(color: Colors.white),
+                Text(
+                  "Special Instructions",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.blueAccent),
+                    // color: Colors.white,
+                  ),
+                  child: TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        )),
+                  ),
+                ),
+                const Divider(color: Colors.white),
+                CheckboxListTileList(checkboxListTileList),
+                const Divider(color: Colors.white),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "Total",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "AED 39",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 123,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        color: Colors.green,
+                        textColor: Colors.white,
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "Checkout",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () => {},
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
