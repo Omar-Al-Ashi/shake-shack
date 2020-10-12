1aa747d63e48a43f0d0694813358c698f9365e9c
Omar <omar.ashi@hotmail.com>
2020-10-12 12:37:07 +0400

    Get data from API in meals overview screen

--- lib/screens/meals_overview_screen.dart
+++ lib/screens/meals_overview_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+import '../api/api.dart';
 import '../widgets/image_header.dart';
 import '../widgets/horizontal_list.dart';
 import '../widgets/most-ordered.dart';
 import '../widgets/burger_card.dart';
-import 'meal_detail_screen.dart';
+import './meal_detail_screen.dart';
 
 class MealsOverview extends StatefulWidget {
   @override
@@ -12,6 +15,38 @@ class MealsOverview extends StatefulWidget {
 }
 
 class _MealsOverviewState extends State<MealsOverview> {
+  List icons = [];
+  List burgers = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    setState(() {
+      isLoading = true;
+    });
+    API.getInitialIcon().then((icons) => {
+          setIcons(icons),
+        });
+    API.getBurgerList().then((burgerList) => {
+          setBurgerList(burgerList),
+        });
+    super.initState();
+  }
+
+  setBurgerList(burgerList) {
+    print(burgerList);
+    setState(() {
+      isLoading = false;
+      burgers = burgerList;
+    });
+  }
+
+  setIcons(iconsProps) {
+    setState(() {
+      icons = iconsProps;
+    });
+  }
+
   cardClicked(burgerName) {
     Navigator.of(context).push(
       MaterialPageRoute(
@@ -22,40 +57,54 @@ class _MealsOverviewState extends State<MealsOverview> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ListView(
-        children: [
-          HorizontalList(),
-          ImageHeader(),
-          MostOrdered(),
-          Container(
-            alignment: Alignment.centerRight,
-            child: BurgerCard(
-                imageLocation: "assets/images/burger2.png",
-                burgerPrice: "AED 39",
-                burgerName: "B-b-bacon!",
-                burgerCaption: "Juicy just got juicier.",
-                cardClicked: cardClicked),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              BurgerCard(
-                  imageLocation: "assets/images/burger4.png",
-                  burgerPrice: "AED 42",
-                  burgerName: "ShackBurger*",
-                  burgerCaption: "Cheesy and Juicy.",
-                  cardClicked: cardClicked),
-              BurgerCard(
-                  imageLocation: "assets/images/burger3.png",
-                  burgerPrice: "AED 39",
-                  burgerName: "B-b-bacon!",
-                  burgerCaption: "Juicy just got juicier.",
-                  cardClicked: cardClicked),
-            ],
-          ),
-        ],
-      ),
-    );
+    return isLoading
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : Container(
+            child: ListView(
+              children: [
+                HorizontalList(icons),
+                ImageHeader(),
+                MostOrdered(),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: BurgerCard(
+                      imageLocation: "assets/images/burger2.png",
+                      burgerPrice: "AED 39",
+                      burgerName: "B-b-bacon!",
+                      burgerCaption: "Juicy just got juicier.",
+                      cardClicked: cardClicked),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ...burgers
+                        .map(
+                          (burger) => BurgerCard(
+                              imageLocation: burger["image"],
+                              burgerPrice: burger["price"],
+                              burgerName: burger["title"],
+                              burgerCaption: burger["description"],
+                              cardClicked: cardClicked),
+                        )
+                        .toList()
+                    //   BurgerCard(
+                    //       imageLocation: "assets/images/burger4.png",
+                    //       burgerPrice: "AED 42",
+                    //       burgerName: "ShackBurger*",
+                    //       burgerCaption: "Cheesy and Juicy.",
+                    //       cardClicked: cardClicked),
+                    //   BurgerCard(
+                    //       imageLocation: "assets/images/burger3.png",
+                    //       burgerPrice: "AED 39",
+                    //       burgerName: "B-b-bacon!",
+                    //       burgerCaption: "Juicy just got juicier.",
+                    //       cardClicked: cardClicked),
+                  ],
+                ),
+              ],
+            ),
+          );
   }
 }
