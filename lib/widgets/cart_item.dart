15bdca766ca25b114b5e6ccfa4328160560048a5
Omar <omar.ashi@hotmail.com>
2020-10-13 23:08:03 +0400

    Replace ListTile with ExpansionTile in cart item widget

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shake_shack/widgets/counter.dart';
 
-class CartItem extends StatelessWidget {
+class CartItem extends StatefulWidget {
   final String image;
   final String burgerName;
   final String price;
@@ -8,26 +9,114 @@ class CartItem extends StatelessWidget {
   CartItem({this.burgerName, this.image, this.price});
 
   @override
+  _CartItemState createState() => _CartItemState();
+}
+
+class _CartItemState extends State<CartItem> {
+  bool isExpanded = false;
+
+  toggleExpandCard() {
+    setState(() {
+      isExpanded != isExpanded;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // return Card(
+    //   shape: RoundedRectangleBorder(
+    //     borderRadius: BorderRadius.circular(20),
+    //   ),
+    //   color: Color(0x99575757),
+    //   child: ListTile(
+    //     onTap: () => toggleExpandCard,
+    //     leading: Container(
+    //       decoration: BoxDecoration(
+    //         borderRadius: BorderRadius.circular(20),
+    //         border: Border.all(color: Colors.white),
+    //       ),
+    //       child: Image.asset(
+    //         widget.image,
+    //         height: 40,
+    //         width: 45,
+    //       ),
+    //     ),
+    //     title: Text(
+    //       widget.burgerName,
+    //       style: TextStyle(
+    //         color: Colors.white,
+    //       ),
+    //     ),
+    //     trailing: Text(
+    //       widget.price,
+    //       style: TextStyle(
+    //         color: Colors.white,
+    //       ),
+    //     ),
+    //   ),
+    // );
     return Card(
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(20),
       ),
       color: Color(0x99575757),
-      child: ListTile(
-        leading: Image.asset(image),
-        title: Text(
-          burgerName,
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
+      child: ExpansionTile(
+        backgroundColor: Color(0x99575757),
         trailing: Text(
-          price,
+          widget.price,
           style: TextStyle(
             color: Colors.white,
           ),
         ),
+        leading: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Colors.white),
+          ),
+          child: Image.asset(
+            widget.image,
+            height: 40,
+            width: 45,
+          ),
+        ),
+        title: Text(
+          widget.burgerName,
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+
+        // subtitle: Text("  Sub Title's"),
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    "No Lettuce",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "No Onions",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "No Tomatoes",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              Counter(),
+            ],
+          )
+        ],
       ),
     );
   }
